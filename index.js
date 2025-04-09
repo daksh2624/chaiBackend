57be94b70706dc6c6c26faf25fa75dbfbdcb4874
Mr-Mehta <dilmehta56@gmail.com>
2025-04-09 22:30:38 +0530

    github data added

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 require('dotenv').config()
+
 const express = require('express')
+
 const app = express()
+
 const port = 5000
+
+const githubData = {
+    "login": "daksh2624",
+    "id": 206917270,
+    "node_id": "U_kgDODFVOlg",
+    "avatar_url": "https://avatars.githubusercontent.com/u/206917270?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/daksh2624",
+    "html_url": "https://github.com/daksh2624",
+    "followers_url": "https://api.github.com/users/daksh2624/followers",
+    "following_url": "https://api.github.com/users/daksh2624/following{/other_user}",
+    "gists_url": "https://api.github.com/users/daksh2624/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/daksh2624/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/daksh2624/subscriptions",
+    "organizations_url": "https://api.github.com/users/daksh2624/orgs",
+    "repos_url": "https://api.github.com/users/daksh2624/repos",
+    "events_url": "https://api.github.com/users/daksh2624/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/daksh2624/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 1,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2025-04-09T16:39:45Z",
+    "updated_at": "2025-04-09T16:40:07Z"
+  }
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -17,6 +56,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) =>{
     res.send("<h2> Chai aur Code </h2>")
+})
+
+app.get('/gitHub', (req, res) =>{
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
